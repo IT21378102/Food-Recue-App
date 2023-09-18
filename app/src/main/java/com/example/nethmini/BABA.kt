887614c9cb5cac91d2b979d8eca8bba42aae9758
Kavindu Herath <99652333+IT21378102@users.noreply.github.com>
2023-09-18 22:41:57 +0530

    Project Added to github

---
+++ app/src/main/java/com/example/nethmini/BABA.kt
@@ -0,0 +1,11 @@
+package com.example.nethmini
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BABA : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_baba)
+    }
+}
